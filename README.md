ea6fb3d6745f4abf6c9939240cf1b84e0ce3b6f3
Brandon <branyzp@gmail.com>
2024-02-04 18:40:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# What's For Lunch - meal picker
+
+## Created to solve one of life's biggest questions
+
+## Technologies used
+
+- React
+- TypeScript
+- Tailwindcss
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			<h1 className="text-xl">What's for lunch?</h1>
+		</div>
+	);
+}
+
+export default App;
